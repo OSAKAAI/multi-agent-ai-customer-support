60ddb183fe689c0beffc18e752162e929a57d442
OSAKAAI <vaibhavpanwargamer1@gmail.com>
2026-07-16 22:39:31 +0530

    AI Integration Phase_1

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.routes import router
 
 app = FastAPI()
 
+# CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
+
 
 @app.get("/")
 def home():
